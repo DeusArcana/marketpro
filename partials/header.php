24e0cb6c51563ed97b217a7e96e1f8f3bc7cc449
Javier Pazos <javier.pazos.ro@gmail.com>
2021-06-25 23:03:04 -0700

    activePage correction

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,6 @@
 <?php 
 	$activePage = str_replace('/', '', dirname($_SERVER['SCRIPT_NAME']));
+
 	?>
 
 <!DOCTYPE html>
@@ -28,7 +29,7 @@
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 							<li class="nav-item">
-								<a class="nav-link <?= ($activePage == 'index') ? 'active':''; ?>" aria-current="page" href="/index.html">Home</a>
+								<a class="nav-link <?= ($activePage == '\\') ? 'active':''; ?>" aria-current="page" href="/index.html">Home</a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link <?= ($activePage == 'offers') ? 'active':''; ?>" href="/offers">Offers</a>
