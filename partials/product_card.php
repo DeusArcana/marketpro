ce1bebb88bd3c3cf2f41d3008c4dffa6815396b9
Javier Pazos <javier.pazos.ro@gmail.com>
2021-06-26 23:55:34 -0700

    Links to cart & view

--- partials/product_card.php
+++ partials/product_card.php
@@ -10,13 +10,13 @@ function product_card($key, $offer)
 {
 	return <<<HTML
 							<div class="col-md-3 pb-3">
-								<div class="card" style="width: 18rem;">
+								<div class="card d-inline" style="width: 18rem;">
 									<img src="{$offer['image']}" class="card-img-top" alt="{$offer['name']}">
 									<div class="card-body">
 										<h5 class="card-title">{$offer['name']}</h5>
 										<p class="card-text">{$offer['intro']}</p>
 										<a href="/offers/offer.html?id={$offer['id']}" class="btn btn-outline-primary">View more details!</a>
-										<a href="/cart/cart.html?id={$offer['id']}&action=add" class="btn btn-outline-sucess">Add to cart!</a>
+										<a href="/cart/cart.html?id={$offer['id']}&action=add" class="btn btn-outline-success">Add to cart!</a>
 									</div>
 								</div>
 							</div>
--- partials/product_detail.php
+++ partials/product_detail.php
@@ -14,7 +14,7 @@ function product_detail($offer) {
 						<hr class="my-4">
 						<p class="lead col-md-8 fs-4">{$offer['intro']}</p>
 						<p class="col-md-8 fs-4">{$offer['description']}</p>
-						<P><a href="#" class="btn btn-outline-success">Antes: {$offer['price']} - <strong>Ahora: {$offer['discount_price']}</strong></a></P>
+						<P><a href="/cart/cart.html?id={$offer['id']}&action=add" class="btn btn-outline-success">Antes: {$offer['price']} - <strong>Ahora: {$offer['discount_price']}</strong></a></P>
 
 HTML;
 }
